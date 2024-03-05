b57dfb8daaee9d2c87322fd4b71ee5d3ce86a5fb
Abdulbasit762 <154410960+Abdulbasit762@users.noreply.github.com>
2024-03-06 03:52:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+
+//  var weekday=prompt("Enter a weekday num")
+//  if(weekday==1){
+//      console.log("monday")
+//  }else if(weekday==2){
+//      console.log("Tuesday")
+//  }else if(weekday==3){
+//     console.log("Wed")
+//  }else if(weekday==4){
+//      console.log("Thursday")
+//  }else if(weekday==5){
+//      console.log("Friday")
+//  }else if(weekday==6){
+//     console.log("Saturday")
+//  }else if(weekday==7){
+//      console.log("sunday")
+//  }else{
+//      console.log("Plzzz enter tha num 1to 7")
+// }
+
+//https://github.com/umarprogrammer19/Javascript-MathExpression-Solves2
+//https://github.com/umarprogrammer19/Javascript-MathExpression-Solves
+//
+
+
+
+// 
+
+///////// assignment no 3
+
+var age = 17 
+
+alert("I am " + age + " year old")
+
+var visit = 10;
+
+alert("You have visited this site"+ visit + "times")
+
+var Birthyear = 2007
+
+document.write("My birth year is " + Birthyear)
+
+ document.write(" Data type of my ddeclared variable is number <br> basit odered" + 5 + " T-shirt(s) on XYZ Clothing store")
+
+// let v1 = 1 , v2 = 2 , v3 = 3;
+
